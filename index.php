64bf4418adf6f01c0a70198bd8b7db7a64d09256
AdrianSzo <adrian.szostek@zsz.bobowa.pl>
2024-10-24 08:52:45 +0200

    Modyfikacja Kodu PHP do Filtrowania

--- index.php
+++ index.php
@@ -25,19 +25,39 @@
     }
     ?>
     <?php 
-    $sql = "SELECT * FROM uczniowie";
+   $servername = "localhost";
+   $username = "root";
+    $password = "";
+    $dbname = "szkola1";
+
+    $conn = mysqli_connect($servername, $username, $password, $dbname);
+
+    if (!$conn) {
+        die("Błąd połączenia: " . mysqli_connect_error());
+    }
+    
+    if(isset($_POST['nazwisko']) && $_POST['nazwisko'] != '') {
+        $nazwisko = $_POST['nazwisko'];
+            // Zabezpieczenie przed SQL Injection
+        $nazwisko = mysqli_real_escape_string($conn, $nazwisko);
+
+        $sql = "SELECT * FROM uczniowie WHERE nazwisko='$nazwisko'";
+    } else {
+        $sql = "SELECT * FROM uczniowie";
+    }
+
     $result = mysqli_query($conn, $sql);
 
     if (mysqli_num_rows($result) > 0) {
-        echo "<table border='1'><tr><th>Imię</th><th>Nazwisko</th><th>Wiek</th><tr>";
+        echo "<table border='1'><tr><th>Imię</th><th>Nazwisko</th><th>Wiek</th></tr>";
         while($row = mysqli_fetch_assoc($result)) {
             echo "<tr><td>".$row["imie"]."</td><td>".$row["nazwisko"]."</td><td>".$row["wiek"]."</td></tr>";
         }
         echo "</table>";
     } else {
-        echo "Brak wynik";
+        echo "Brak wyników";
     }
     mysqli_close($conn);
-    ?>
-</body>
+?>
+    </body>
 </html>
